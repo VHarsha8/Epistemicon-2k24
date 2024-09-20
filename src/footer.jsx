0a9b279b73aeb0ca0735636e4bd6ba7456235029
VHarsha8 <harshavardhan6304@gmail.com>
2024-09-21 04:06:30 +0530

    redesign

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,8 +1,9 @@
 function Footer(){
     return(
-        <div className="bg-darkgreybackground h-auto w-full py-4  md:p-10 flex flex-col  ">
+        <div className="bg-darkgreybackground h-auto w-full py-10  md:p-10 flex flex-col  ">
 
             <div className="container mx-auto flex flex-col px-6 md:px-16">
+                <img src="alietlogo.png" className="w-20"/>
             <h1 className="font-Vogue text-white text-3xl text-left md:text-4xl font-medium py-6 md:py-10">Epistemicon - 2K24</h1>
             <div className="flex md:flex-row flex-col justify-between ">
                 <div className="flex  gap-4  md:gap-10">
