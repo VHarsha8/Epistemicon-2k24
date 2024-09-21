7a5055b8ab8ba0a9fc962682298cb89595dc88e8
VHarsha8 <harshavardhan6304@gmail.com>
2024-09-21 15:30:53 +0530

    Vercel last miles

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 function Timer() {
-  const targetDate = new Date('2024-09-28T00:00:00'); // Set target date
   const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
+    const targetDate = new Date('2024-09-28T00:00:00'); // Set target date here
     const interval = setInterval(() => {
       const now = new Date();
       const difference = targetDate - now;
@@ -24,10 +24,10 @@ function Timer() {
     }, 1000);
 
     return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, [targetDate]);
+  }, []); // No dependencies here
 
   return (
-    <div className="flex flex-col items-center border justify-center py-2 px-4   md:p-4 bg-gray-100  rounded-full shadow-lg">
+    <div className="flex flex-col items-center border justify-center py-2 px-4 md:p-4 bg-gray-100 rounded-full shadow-lg">
       <div className="flex gap-2">
         <div className="flex flex-col items-center">
           <span className="md:text-xl text-md font-AmericanCaptainPatrius">{timeLeft.days} :</span>
@@ -36,10 +36,10 @@ function Timer() {
           <span className="md:text-xl text-md font-AmericanCaptainPatrius">{timeLeft.hours} :</span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="md:text-xl text-md font-AmericanCaptainPatrius ">{timeLeft.minutes}:</span>
+          <span className="md:text-xl text-md font-AmericanCaptainPatrius">{timeLeft.minutes}:</span>
         </div>
         <div className="flex flex-col items-center">
-          <span className="md:text-xl text-md font-AmericanCaptainPatrius ">{timeLeft.seconds}</span>
+          <span className="md:text-xl text-md font-AmericanCaptainPatrius">{timeLeft.seconds}</span>
         </div>
       </div>
     </div>
